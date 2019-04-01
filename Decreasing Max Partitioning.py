327ba836f2bd46b9d0ab401c607c53bdc49d0ff6
AVIJIT DAS <davijit868@gmail.com>
2019-04-01 17:09:51 +0530

    Decreasing Max Partitioning
    Not all test cases are passing.

---
+++ Decreasing Max Partitioning.py
@@ -0,0 +1,34 @@
+c = 0
+def partition(a, t):
+    global c, sg
+    for i in range(1,len(a)+1):
+        t2 = t[:]
+        temp = a[:i]
+        
+        if len(t) == 0:
+            t.append(temp)
+            if len(a[i:]) == 0:
+                c += 1
+                #print(t)
+            partition(a[i:],t)
+            t = t2
+        
+        elif max(t[-1]) >= max(temp):
+            t.append(temp)
+            if len(a[i:]) == 0:
+                c += 1
+                #print(t)
+            partition(a[i:],t)
+            t = t2
+       
+        
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int,input().split()))
+    partition(a,[])
+# for i in g:
+#     print(i)
+    print(c % 1000000007)
+    c = 0
