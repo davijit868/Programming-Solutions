44ef792d7862966c9efb01c1510c8dd83930a8a0
Avijit Das <davijit868@gmail.com>
2019-06-18 20:01:12 +0530

    Pseudocode

--- Algorithms/Graphs/Breadth First Search/Breadth First Search.py
+++ Algorithms/Graphs/Breadth First Search/Breadth First Search.py
@@ -1,3 +1,15 @@
-# Pseudocode 
+# Pseudocode for Breadth First Search Algorithm
 
-BFS(G,s)
+BFS (G,s)
+	let Q be Queue.
+	Q.enqueue(s) 
+	mark s as visited.
+	print(s)
+	while ( Q is not empty )
+		v = Q.dequeue()
+		for all neighbours w of v in graph G:
+			if w is not visited.
+				print(w)
+				Q.enqueue(w)
+				mark w as visited.
+		
