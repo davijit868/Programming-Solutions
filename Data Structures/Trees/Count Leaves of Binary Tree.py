62693fc7d08964ea2bf263eafae68157afce1922
Avijit Das <davijit868@gmail.com>
2019-07-18 19:55:59 +0530

    Update Count Leaves of Binary Tree.py

--- Data Structures/Trees/Count Leaves of Binary Tree.py
+++ Data Structures/Trees/Count Leaves of Binary Tree.py
@@ -1,4 +1,8 @@
+'''
 
+https://practice.geeksforgeeks.org/problems/count-leaves-in-binary-tree/1
+
+'''
 def countLeaves(root):
     if not root: return 0
     if not root.left and not root.right: return 1
