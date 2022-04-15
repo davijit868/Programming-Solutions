ed1c70101da99c17bb301f2cc8026e07657eaa0b
Avijit Das <davijit868@gmail.com>
2022-04-15 19:15:37 +0100

    Create Jump Game Linear.py

---
+++ Data Structures/Arrays/Jump Game Linear.py
@@ -0,0 +1,15 @@
+'''
+
+Problem 55 | Jump Game
+https://leetcode.com/problems/jump-game/
+
+'''
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        return True if goal == 0 else False
+                
